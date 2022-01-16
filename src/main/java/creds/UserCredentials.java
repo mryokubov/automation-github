569abbf44d34ed63d06158243913fabbdb5ffc8a
mryokubov <97797362+mryokubov@users.noreply.github.com>
2022-01-16 12:01:31 -0500

    updated user credentials file

--- src/main/java/creds/UserCredentials.java
+++ src/main/java/creds/UserCredentials.java
@@ -6,4 +6,5 @@ package creds;
 public class UserCredentials {
     public static final String OLD_PASSWORD="user111";
     public static final String NEW_PASSWORD="user112";
+    public static final String SOMETHING="";
 }
